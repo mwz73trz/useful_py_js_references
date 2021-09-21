84e746be3b993ec9543be894059b56b574c97425
mwz73trz <michaelzura@bellsouth.net>
2021-09-21 13:49:26 -0400

    added stacks

---
+++ python/Stacks/collections.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+stack = deque()
+
+stack.append(1)
+stack.append(2)
+stack.append(3)
+
+print(stack)
+
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print(stack)
---
+++ python/Stacks/stacks.py
@@ -0,0 +1,17 @@
+# stacks follow Last In First Out (LIFO) 
+
+stack = []
+
+stack.append(1)
+print(stack)
+
+stack.append(2)
+print(stack)
+
+stack.append(3)
+print(stack)
+
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack)
